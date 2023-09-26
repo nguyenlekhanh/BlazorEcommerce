e2a520f5ecdf5cd6b8ab60210ca1bb2b0e722971
Khanh Nguyen <nguyenlekhanh811@gmail.com>
2023-09-26 13:26:16 -0500

    install microsoft entity framework add ConnectionString add DataContext

---
+++ BlazorEcommerce/Server/Controllers/Data/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using BlazorEcommerce.Shared;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlazorEcommerce.Server.Controllers.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> option) : base(options)
+        {
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
