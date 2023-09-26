306b9ed9ed54f9b2cd3ef117e7726493080bd847
Khanh Nguyen <nguyenlekhanh811@gmail.com>
2023-09-26 16:47:41 -0500

    Add Category Service to Client Show categories on Navbar

---
+++ BlazorEcommerce/Client/Services/CategoryService/CategoryService.cs
@@ -0,0 +1,27 @@
+﻿using BlazorEcommerce.Shared;
+using System.Net.Http.Json;
+using static System.Net.WebRequestMethods;
+
+namespace BlazorEcommerce.Client.Services.CategoryService
+{
+    public class CategoryService : ICategoryService
+    {
+        private readonly HttpClient _http;
+
+        public CategoryService(HttpClient http)
+        {
+            _http = http;
+        }
+
+        public List<Category> Categories { get; set; } = new List<Category>();
+
+        public async Task GetCategories()
+        {
+            var result = await _http.GetFromJsonAsync<ServiceResponse<List<Category>>>("api/Category");
+            if (result is not null)
+            {
+                Categories = result.Data;
+            }
+        }
+    }
+}
