b72b343b7740d623a7760ece46ecb28ff11e4995
Khanh Nguyen <nguyenlekhanh811@gmail.com>
2023-09-27 14:45:30 -0500

    add HomeButton for redirect to HomePage add Feature field to Product model

--- BlazorEcommerce/Shared/Product.cs
+++ BlazorEcommerce/Shared/Product.cs
@@ -18,6 +18,8 @@ namespace BlazorEcommerce.Shared
         public Category Category { get; set; }
         public int CategoryId { get; set; }
 
+        public bool Featured { get; set; } = false;
+
         public List<ProductVariant> Variants { get; set; } = new List<ProductVariant>();
 
 
