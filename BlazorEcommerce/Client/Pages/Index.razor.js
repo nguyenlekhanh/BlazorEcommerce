4dbd59ffc94c42f83a25b3118ec3a7fd01b31f2b
Khanh Nguyen <nguyenlekhanh811@gmail.com>
2023-09-26 23:00:50 -0500

    first implement javascript

---
+++ BlazorEcommerce/Client/Pages/Index.razor.js
@@ -0,0 +1,7 @@
+﻿export function showPrompt(message) {
+    return prompt(message, 'Type anything here');
+}
+export function AddViewer(id) {
+    //var viewer = new foo.Viewer(id, /*other params*/);
+    /* ...Your code... */
+}
